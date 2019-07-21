d87a2d3339eb94e9f14cf505873eb0694d7c660d
DoHwanYoon <heesosik4you@naver.com>
2019-07-21 23:04:10 +0900

    dataframe

---
+++ dataframe.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+data = {'가격':[3000,2000,1000], '수량':[3,3,2]}
+testdata = pd.DataFrame(data, index=['자몽', '수박', '사과'])
+
+#testdata.to_excel('testdata.xlsx')
+
+re=pd.read_excel('testdata.xlsx')
+print(re)
