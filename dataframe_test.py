347aa48a36ead1c3ccd3cf4f2f8b3092c630e230
DoHwanYoon <heesosik4you@naver.com>
2019-07-23 15:05:45 +0900

    수정

--- dataframe_test.py
+++ dataframe_test.py
@@ -4,7 +4,7 @@ import pandas as pd
 data = {'가격':[3000,2000,1000], '수량':[3,3,2]}
 testdata = pd.DataFrame(data, index=['자몽', '수박', '사과'])
 
-testdata.to_excel('testdata.xlsx')
+#testdata.to_excel('testdata.xlsx')
 
 re=pd.read_excel('testdata.xlsx')
 print(re)
--- input_output.py
+++ input_output.py
@@ -2,9 +2,9 @@ import openpyxl
 
 wb = openpyxl.load_workbook('test.xlsx')
 sheet = wb['Sheet1']
+   
+   
+for i in range(5):
+    print(sheet['A+i'].value)
 
-for row in sheet.rows:
-    for cell in row:
-        print(cell.value, end=' ')
-    print()    
-#print(sheet['A1'].value)
+print(sheet['A1'].value)
