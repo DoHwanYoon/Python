8d0468100f335b415ab44426fa99c5dac30c17f3
DoHwanYoon <heesosik4you@naver.com>
2019-09-16 17:30:20 +0900

    saved

--- bayes_excel_4.py
+++ bayes_excel_4.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Output all contents of file.
 excel_file = pd.read_excel('test.xlsx', sheet_name='Sheet1')
 
-load_wb = load_workbook('test.xlsx', data_only=False)    # You must "data_only = True". If not this, you are see function value
+load_wb = load_workbook('test.xlsx', data_only=False)
 load_ws = load_wb['Sheet1']                             # Enter Sheet Name
 rows = load_ws.max_row + 1                              # Number of rows (add title rows)
 cols = load_ws.max_column + 1                           # Number of colmn (add title rows)
